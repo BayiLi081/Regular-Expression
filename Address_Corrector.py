b1c3fcf90d6a8b042094fc772967791f4ab49715
Bayi Li <barryli081@gmail.com>
2022-02-23 21:54:40 +0000

    First Address Format Corrector

---
+++ Address_Corrector.py
@@ -0,0 +1,34 @@
+import re
+from titlecase import titlecase
+import webbrowser
+while True:
+    out = input("Please enter a Sentence:\n")
+
+    # remove multiple spaces
+    out = out.strip()
+    out = " ".join(out.split())
+    
+    # add a space after comma
+    out = re.sub(r'(?<=[,])(?=[^\s])', r' ', out)
+
+    # Capitalize first character in every word
+    out_tle = out.title()
+
+    # Covert the given text to Title Caps, but avoid to cap small words like a/an/the/of
+    # drop the end period at the end of text
+    out_tle = titlecase(out_tle).rstrip('.').rstrip().rstrip(',')
+    print(out_tle)
+    # remove the space before a comma
+    out_tle = re.sub(r'\s*,', ',', out_tle)
+
+    # print new sentence
+    print('New sentence is:\n',out_tle)
+
+    # automatically open google search in broswer and search the address to find out if the address is spelled correctly
+    url = "https://www.google.com.tr/search?q={}".format(out_tle)
+    webbrowser.open_new_tab(url)
+
+    if out == 'q': # break the loop
+        break
+print('Loop exited')
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+1. Address_Corrector
+
+   Example Address for testing: "50 Quen  rOAD   , rEADINg,  .   "
+
+   Output String: "50 Quen Road, Reading"
+
+   ![plot](./images/Address_Corrector_01.jpg)
